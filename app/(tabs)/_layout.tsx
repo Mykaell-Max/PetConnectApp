08c2e077c6ea317bf3d40523e4f9f67a70afc8d5
Mykaell Max <max000.256.000@gmail.com>
2024-10-15 15:51:54 -0300

    Initial structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,27 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+        tabBarActiveTintColor: '#ffd33d',
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+        backgroundColor: '#25292e',
+        },
+      }}
+    >
+      
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="(chat)/chatsPreview" options={{ title: 'Chats' }} />
+      <Tabs.Screen name="(user)/userProfile" options={{ title: 'Profile' }} />
+      <Tabs.Screen name="(auth)/login" options={{ href: null }} />
+      <Tabs.Screen name="(auth)/register" options={{ href: null }} />
+
     </Tabs>
   );
 }
